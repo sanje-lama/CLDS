fd0ffdfa651e225e30e61656f4062307083fab7f
sanje-lama <sanjelama@pursuit.org>
2018-12-05 17:32:34 -0500

    fixed master

--- app/src/main/java/com/example/stl/clds/MainActivity.java
+++ app/src/main/java/com/example/stl/clds/MainActivity.java
@@ -9,15 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-
-
-
-
     }
-
-
-=======
-    }
->>>>>>> bf011dad7b5f4549e1f333b3e92c6e74d7f9533c
 }
