9eae660a55b959c11acf87e3839d21dcbca33a93
garlicfiendck <carolinekang@pursuit.org>
2018-12-06 13:53:51 -0500

    Template

--- app/src/main/java/com/example/stl/clds/MainActivity.java
+++ app/src/main/java/com/example/stl/clds/MainActivity.java
@@ -1,15 +1,30 @@
-package com.example.stl.clds;
+package com.example.carolinekang.drafttwoclds;
 
+import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import java.util.Random;
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hello from hyunki
-        //hello from caroline
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
     }
 }
